d1e67a162b4ea16c7b29f3fda0c12a08ecd23514
Vicky-2005 <vikastarange7@gmail.com>
2024-02-28 00:21:05 +0530

    created and updated code

---
+++ Hfunction.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+String DecimalToHexadecimal(int n){
+    
+ 
+} 
+int main(){
+    String n;
+    cin>>n;
+
+    cout << DecimalToHexadecimal(n)<<endl;
+
+}
---
+++ if else.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    
+    
+    if(n>99  and  n<1000){
+        cout<<"3 digit number"<<endl;
+    }
+    else{
+        cout<<"Not a 3 digit number"<<endl;
+    }
+    return 0;
+}
